253bf4549b742b3a873d5102b3e8eda417f9f8c7
Amogh J <amoghjahagirdargsf@gmail.com>
2020-09-24 08:07:30 +0530

    Setup

---
+++ example/pageview_parallax_example.dart
@@ -0,0 +1,6 @@
+import 'package:pageview_parallax/pageview_parallax.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/pageview_parallax.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library pageview_parallax;
+
+export 'src/pageview_parallax_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
